d59de074c39b5477a0a5681443eb1c2fbf40dfaa
RORTEGA <rojosormi@gmail.com>
2025-08-07 22:19:28 -0500

    Desarrollando funcionalidades para el Juego del Amigo Secreto

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # challenge-amigo-secreto_esp-main
 Challenge AluraLatan Oracle
--#Tablero Trello(https://trello.com/b/zRGwOPSC/trello-challenge-amigo-secreto-esp)
+
+Tablero
+[Trello][def]
+
+
+
+[def]: https://trello.com/b/zRGwOPSC/trello-challenge-amigo-secreto-esp
--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosSecretos = [];
+
+
+function sortearAmigo(){
+    sortearAmigo= amigosSecretos[Math.floor(Math.random() * amigosSecretos.length)];
+    return amigosSecretos[sortearAmigo];    
+}
+function limpiartexto(){
+    document.getElementById("amigo").value = "";
+    return;
+}
+function agregarAmigo(){
+    amigosSecretos.push(document.getElementById("amigo").value);
+    limpiartexto();
+    return amigosSecretos;
+}
+function sortear(){
+    
+}
